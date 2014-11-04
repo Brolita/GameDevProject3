ed74175715c8416c0185e35453d3de33f007aab4
brolita <byrnec3@rpi.edu>
2014-11-04 00:04:18 -0500

    Where did they go ? :(

---
+++ Assets/RemoveSpikes.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveSpikes : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider other) 
+	{
+		print(transform.position.y);
+		if(other.tag == "Remover")
+		{
+			Transform[] children = GetComponentsInChildren<Transform>();
+			for(int i = 0; i < children.Length; i++) 
+			{
+				if(children[i].tag == "Spike")
+					Destroy(children[i].gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/Remover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Remover : MonoBehaviour {
+	int frame;
+	void Start () 
+	{
+		frame = 0;
+	}
+	void Update () 
+	{
+		frame ++;
+		if (frame == 6) Destroy (gameObject);
+	}
+}
