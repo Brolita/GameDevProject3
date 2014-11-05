80dc28a38d55807acbe19758a894e1c63ede47e5
Parshva Shah <g7parsh@gmail.com>
2014-11-05 04:54:09 -0500

    back to square -1

---
+++ Assets/BackgroundThings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundThings : MonoBehaviour {
+	public GameObject ring;
+	public GameObject cone;
+	public GameObject rib;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
